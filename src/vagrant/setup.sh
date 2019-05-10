1387cdced7439faf746d5a45d007cba990aba60b
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-05-10 18:50:55 +0900

    Added copyright header
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

--- src/vagrant/setup.sh
+++ src/vagrant/setup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# This file is part of the Volta Project.
+#
+# Copyright (c) 2018 - 2019 AzuyaLabs
+#
+# License AGPL-3.0
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author Sacha Telgenhof <me at sachatelgenhof dot com>
 sudo apt-get update
 sudo apt-get install -y gawk util-linux realpath git qemu-user-static p7zip-full unzip zip
 
